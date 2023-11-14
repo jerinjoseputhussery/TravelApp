8cb947cec34b6a4954fed31b5a29f0cfc71dc431
jerinjoseputhussery <60233550+jerinjoseputhussery@users.noreply.github.com>
2023-11-14 15:36:36 +0000

    Package APIs added
    Package APIs added

--- backend/routes/packageRoute.js
+++ backend/routes/packageRoute.js
@@ -44,7 +44,46 @@ packageRoute.post('/add', async(request,response)=>{
       response.status(500).send({message:error.message});
     }
   })
-  
+  packageRoute.get('/:id', async(request,response)=>{
+    try {
+      const {id}=request.params;
+      const packages=await Packages.findById(id);
+      
+      
+        return response.status(200).send(packages);
+      
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({message:error.message});
+    }
+  })
+  packageRoute.post('/edit/:id', async(request,response)=>{
+    try {
+      
+      if(!request.body.title ||
+        !request.body.description ||
+        !request.body.country ||
+        !request.body.noOfDays||
+        !request.body.images||
+        !request.body.rate){
+          return response.status(400).send({
+            message: 'Send all required fields: title,description,country,noOfDays,images,rate,location(optional)',
+          });
+        }
+
+        const {id}=request.params;
+      const result=await Packages.findByIdAndUpdate(id,request.body);
+        if(!result){
+          return response.status(404).send({'message':'Package not found'});
+        }
+
+        return response.status(201).send({'message':'Package updated successfully','status':0,result});
+      
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({message:error.message});
+    }
+  })
   
 
   export default packageRoute;
