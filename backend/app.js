e4dbf28e2e17c27c56ac4785bdd68d212766a6fc
jerinjoseputhussery <60233550+jerinjoseputhussery@users.noreply.github.com>
2023-11-28 00:48:18 +0000

    CORS policy changes3
    CORS policy changes3

--- backend/app.js
+++ backend/app.js
@@ -40,9 +40,9 @@ const store = new MongoDBSession({
 //     origin: 'https://adventureallybooking.onrender.com/',
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 //   }
-// app.use(cors(corsOptions));
+app.use(cors());
 
-// app.use(helmet.frameguard())
+app.use(helmet.frameguard())
 // app.use(
 //   helmet({
 //     crossOriginResourcePolicy: false,
